e0b0c3fe109694247986170fb55c1378915ed0c7
lechan <lechan@126.com>
2023-10-10 16:43:18 +0800

    fix: 处理样式问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://glittery-palmier-8b1821.netlify.app/',
+  integrations: [preact()]
+});
